6b0b81b24986845221dc749950556f654fd92601
Jason DiBabbo <jdibabbo@gmail.com>
2013-07-19 16:13:53 -0500

    Refactored a whole bunch of data objects. Everything should be a lot cleaner now.

---
+++ CoyoteMoves/Models/RequestItems/IndividualRequest.cs
@@ -0,0 +1,22 @@
+﻿using CoyoteMoves.Models.EmployeeData;
+using CoyoteMoves.Models.SeatingData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CoyoteMoves.Models.RequestItems
+{
+    public class IndividualRequest
+    {
+        public Employee CurrentEmployee { get; set; }
+        public Desk CurrentDesk { get; set; }
+        public RequestFutureProperties FutureDetails { get; set; }
+        public DateTime DateToOccurOn { get; set; }
+        public List<string> EmailListsToAddTo { get; set; }
+        public List<string> EmailListsToBeRemovedFrom { get; set; }
+        public string CurrentOffice { get; set; }
+        public string UltiProComment { get; set; }
+        public string SecurityItemRights { get; set; }
+    }
+}
--- CoyoteMoves/Models/SeatingData/CoordinatePoint.cs
+++ CoyoteMoves/Models/SeatingData/CoordinatePoint.cs
@@ -1,19 +1,22 @@
-﻿using CoyoteMoves.Models.Enums;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace CoyoteMoves.Models.SeatingData
 {
-    public class Location
+    public class CoordinatePoint
     {
         public double XCoordinate { get; set; }
         public double YCoordinate { get; set; }
-        public Floor Floot { get; set; }
-        public Conference Conference { get; set; }
 
-        public Location()
+        public CoordinatePoint(double x, double y)
+        {
+            this.XCoordinate = x;
+            this.YCoordinate = y;
+        }
+
+        public CoordinatePoint()
         {
             this.XCoordinate = 0.0;
             this.YCoordinate = 0.0;
