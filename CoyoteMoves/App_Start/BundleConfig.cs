6f72b8bc7f9ae280e409dde2f7b725cc587e7f48
Jake Marsh <jakemmarsh@gmail.com>
2013-07-19 08:44:56 -0500

    removed more unnecessary files

--- CoyoteMoves/App_Start/BundleConfig.cs
+++ CoyoteMoves/App_Start/BundleConfig.cs
@@ -8,11 +8,6 @@ namespace CoyoteMoves
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/todo").Include(
-                "~/Public/js/app/todo.bindings.js",
-                "~/Public/js/app/todo.datacontext.js",
-                "~/Public/js/app/todo.model.js",
-                "~/Public/js/app/todo.viewmodel.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
