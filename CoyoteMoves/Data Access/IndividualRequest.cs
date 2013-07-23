d1531c735a12354bdc21e344cf4f594b87cf688a
Kevin Jasieniecki <jasieni2@illinois.edu>
2013-07-23 13:03:09 -0500

    babby's first database call

---
+++ CoyoteMoves/Data Access/IndividualRequest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace CoyoteMoves.Data_Access
+{
+    public class IndividualRequest
+    {
+        string connectionString = "server=AnalyticsProd;database=Intern_CoyoteMoves;User Id=Intern;Password=Intern2013!;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False";
+
+        public IndividualRequest()
+        {
+           // connectionString = (string)System.Web.Configuration.WebConfigurationManager.ConnectionStrings["DataClientRead"].ConnectionString;
+
+        }
+
+        public int test()
+        {
+            SqlConnection myConnection = new SqlConnection(connectionString);
+            string testQuery = "SELECT TOP 1 [EmployeeID] FROM [Intern_CoyoteMoves].[dbo].[InternalEmployee]";
+            SqlCommand myCommand = new SqlCommand(testQuery);
+            myCommand.Connection = myConnection;
+            myConnection.Open();
+            int pleasebe46 = (int)myCommand.ExecuteScalar();
+            myConnection.Close();
+
+            return pleasebe46;
+
+        }
+
+    }
+}
---
+++ CoyoteMovesTest/SqlConnectionTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CoyoteMoves;
+using CoyoteMoves.Data_Access;
+
+namespace CoyoteMovesTest
+{
+    [TestClass]
+    public class SqlConnectionTest
+    {
+        IndividualRequest _requester = new IndividualRequest();
+
+        [TestMethod]
+        public void testTest_testDotTest()
+        {    
+            int pleasebe46;
+            pleasebe46 = _requester.test();
+            
+            Assert.AreEqual(46, pleasebe46);
+        }
+    }
+}
