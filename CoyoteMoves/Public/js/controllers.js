79d8d31df885e7a15f12b8871bd6af7d1c9fd262
Brandon Dsouza <bdsouza39@gmail.com>
2013-07-23 15:31:29 -0500

    Starting coyote moves form

--- CoyoteMoves/Public/js/controllers.js
+++ CoyoteMoves/Public/js/controllers.js
@@ -1,4 +1,24 @@
 ﻿function IndexCtrl($scope, $routeParams) {
     $scope.currentFloor = 3;
 
-}
+    $scope.currentEmployee = 0;
+
+    $scope.changeCurrentForm = function (index) {
+        $scope.currentEmployee = index;
+        $scope.$apply();
+        console.log(index);
+    }
+
+    $scope.displacedEmployees = [{
+        name: "Jake"
+    },
+    {
+        name: "Magic"
+    },
+    {
+        name: "Brandon"
+    },
+    {
+        name: "Ian"
+    }];
+};
