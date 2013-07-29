c071d80eb4a2240ba1cfc6b011fd1d1530dc7167
Jason DiBabbo <jdibabbo@gmail.com>
2013-07-29 08:20:48 -0500

    Refactored a couple of the request data classes and added a new data DB connection class to get certain drop down field data like Departments, Groups and JobTitles.

--- CoyoteMoves/Models/RequestItems/RequestTypes/EmailDistributionChangeRequest.cs
+++ CoyoteMoves/Models/RequestItems/RequestTypes/EmailDistributionChangeRequest.cs
@@ -8,8 +8,8 @@ namespace CoyoteMoves.Models.RequestItems.RequestTypes
     public class EmailDistributionChangeRequest
     {
         //not sure how exactly we plan on doing this, but a list of strings seems like a good way...
-        public List<string> GroupsToAddPersonTo { get; set; }
-        public List<string> GroupsToRemovePersonFrom { get; set; }
+        public List<string> GroupsToBeAddedTo { get; set; }
+        public List<string> GroupsToBeRemovedFrom { get; set; }
 
     }
 }
