a0bb6ccbfe4a29b66abc66c0e560cd4498ac89c2
MythicHelm <mjhymel111@gmail.com>
2013-07-31 10:11:15 -0500

    Started working on a function to read the google doc data with names and desk numbers into the database

---
+++ CoyoteMoves/Data Access/InitializeDeskDB.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace CoyoteMoves.Data_Access
+{
+    public class InitializeDeskDB
+    {
+
+        public string _connectionString { get; set; }
+
+        public InitializeDeskDB()
+        {
+            this._connectionString = (string)System.Web.Configuration.WebConfigurationManager.ConnectionStrings["DataClientRead"].ConnectionString;
+        }
+
+        public void AddNamesAndDeskNumbersFromFile(string filePath)
+        {
+            
+        }
+
+        private void InsertInformationIntoDeskDB(string deskNumber, int topLeftX, int topLeftY, int orientation, int floorNumber, int employeeID)
+        {
+            SqlConnection connection = new SqlConnection(this._connectionString);
+            string commandString = "INSERT INTO dbo.Desk (DeskNumber, TopLeftX, TopLeftY, Orientation, FloorNumber, EmployeeID)";
+            commandString += " Values (" + "'" + deskNumber + "'" + ", " + topLeftY + ", " + orientation + ", " + floorNumber + ", " + employeeID + ")";
+            SqlCommand command = new SqlCommand(commandString);
+            command.Connection = connection;
+            command.Connection.Open();
+            int result = command.ExecuteNonQuery();
+            command.Connection.Close();
+            if (result != 1)
+            {
+                //error
+            }
+        }
+    }
+}
---
+++ CoyoteMovesTest/InitializeDeskDBTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CoyoteMovesTest
+{
+    [TestClass]
+    public class InitializeDeskDBTest
+    {
+        [TestMethod]
+        public void TestAddNamesAndDeskNumbersFromFile()
+        {
+
+        }
+    }
+}
