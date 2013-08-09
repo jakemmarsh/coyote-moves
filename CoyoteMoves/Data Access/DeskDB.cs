4f9104ba034214a9bc5b6c0c62128664f8cef977
Jason DiBabbo <jdibabbo@gmail.com>
2013-08-09 13:21:52 -0500

    Made an endpoint to get all desks at once.

--- CoyoteMoves/Data Access/DeskDB.cs
+++ CoyoteMoves/Data Access/DeskDB.cs
@@ -49,7 +49,32 @@ namespace CoyoteMoves.Data_Access
                 throw new Exception(ex.Message);
             }
         }
-        
+
+        public List<Desk> GetAllDesks()
+        {
+            List<Desk> deskList = new List<Desk>();
+            SqlConnection connection = new SqlConnection(_connectionString);
+            string commandString = "EXEC dbo.spDesk_GetAllDesks";
+            SqlCommand command = new SqlCommand(commandString);
+
+            try
+            {
+                command.Connection = connection;
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                SqlToDeskModelFactory DeskFactory = new SqlToDeskModelFactory(reader);
+                deskList = DeskFactory.GetAllDesks();
+                connection.Close();
+                return deskList;
+            }
+
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
         public void AddNamesAndDeskNumbersFromFile(string filePath)
         {
             //this should take a csv file
